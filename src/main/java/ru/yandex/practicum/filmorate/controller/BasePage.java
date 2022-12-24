5173d4341dc92256d83647eea036830bc9ce850b
Artem Shestakov <shestakov_art@vk.com>
2022-12-24 18:10:35 +0500

    add testFixhost

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/BasePage.java
@@ -0,0 +1,17 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@Slf4j
+@RestController
+@RequestMapping("")
+public class BasePage {
+
+    @GetMapping()
+    public void get(){
+    }
+}
