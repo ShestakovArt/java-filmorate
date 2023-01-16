d5b62eb5be7ae797d4d2cfacc138c5cbb79c6796
yabro <yabrow@yandex.ru>
2023-01-16 18:17:11 +0500

    feat: created methods providing films search by %phrase% pattern with director_name & film_title fields

--- src/main/java/ru/yandex/practicum/filmorate/dao/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/FilmDbStorage.java
@@ -2,7 +2,6 @@ package ru.yandex.practicum.filmorate.dao;
 
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.model.Film;
-import ru.yandex.practicum.filmorate.model.Genre;
 
 import java.util.Collection;
 import java.util.List;
@@ -26,4 +25,8 @@ public interface FilmDbStorage {
     boolean deleteLike(Integer idFilm, Integer idUser);
 
     Collection<Film> findDirectorSortedFilms(Integer directorId, String[] sortBy);
+
+    Collection<Film> findFilmsByDirector(String criteria);
+
+    Collection<Film> findFilmsByTitle(String criteria);
 }
