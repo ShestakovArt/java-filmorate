2032c9a2dd0797c8f10fd8084f58060aa7145ed2
yabro <yabrow@yandex.ru>
2023-01-22 22:46:35 +0500

    fix: удалил ненужные аннотации @Component над интерфейсами

--- src/main/java/ru/yandex/practicum/filmorate/dao/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/FilmDbStorage.java
@@ -1,15 +1,13 @@
 package ru.yandex.practicum.filmorate.dao;
 
-import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.model.Film;
 
 import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
-
-@Component
 public interface FilmDbStorage {
+
     int addFilm(Film film);
 
     void upgradeFilm(Film film);
